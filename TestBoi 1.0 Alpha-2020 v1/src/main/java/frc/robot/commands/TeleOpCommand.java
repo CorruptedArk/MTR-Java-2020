c4b40117d69bf76d831f449371bbad45723286f6
Scriptyure <42632016+Hasplit@users.noreply.github.com>
2020-01-07 17:25:50 -0500

    Add files via upload
    All changes done in robotics, this includes fixing mechanim and other changes

--- TestBoi 1.0 Alpha-2020 v1/src/main/java/frc/robot/commands/TeleOpCommand.java
+++ TestBoi 1.0 Alpha-2020 v1/src/main/java/frc/robot/commands/TeleOpCommand.java
@@ -9,6 +9,8 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.RobotContainer;
 
 
--- TestBoi 1.0 Alpha-2020 v1/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ TestBoi 1.0 Alpha-2020 v1/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -14,29 +14,32 @@ import frc.robot.RobotContainer;
 import edu.wpi.first.wpilibj.Talon;
 
 public class DriveSubsystem extends SubsystemBase {
-  private final int frontRightMotor;
-  private final int rearRightMotor;
-  private final int frontLeftMotor;
-  private final int rearLeftMotor;
-  private final int chassis;
+  private final Talon frontRightMotor;
+  private final Talon rearRightMotor;
+  private final Talon frontLeftMotor;
+  private final Talon rearLeftMotor;
+  private final MecanumDrive chassis;
+ 
   
   /**
    * Creates a new DriveSubsystem.
    */
+  //DriveSystem Constuctor
   public DriveSubsystem() {
-    final Talon frontRightMotor = new Talon(Constants.FRONT_RIGHT_MOTOR_ID);
-    final Talon frontLeftMotor = new Talon(Constants.FRONT_LEFT_MOTOR_ID);
-    final Talon rearRightMotor = new Talon(Constants.REAR_RIGHT_MOTOR_ID);
-    final Talon rearLeftMotor = new Talon(Constants.REAR_LEFT_MOTOR_ID);
-    final MecanumDrive chassis = new MecanumDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+    //link motors to the const/final 
+    frontRightMotor = new Talon(Constants.FRONT_RIGHT_MOTOR_ID);
+    frontLeftMotor = new Talon(Constants.FRONT_LEFT_MOTOR_ID);
+    rearRightMotor = new Talon(Constants.REAR_RIGHT_MOTOR_ID);
+    rearLeftMotor = new Talon(Constants.REAR_LEFT_MOTOR_ID);
+    chassis = new MecanumDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+  }
+  
 
-    public void drive(double frontRightSpeed, double frontLeftSpeed, double rearRightSpeed, double rearLeftSpeed) {
-      chassis.MecanumDrive(frontRightSpeed, frontLeftSpeed, rearRightSpeed);
-
-    }
+  
+  public void drive(final double rightSpeed, final double leftSpeed) {
+      chassis.driveCartesian(rightSpeed, leftSpeed, 0,0);
 
   }
-
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
